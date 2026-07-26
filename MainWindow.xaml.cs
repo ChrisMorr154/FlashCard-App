10d6ca885136366c2a24d37ba825fc231a3bea3e
Christian <christianmorris154@gmail.com>
2026-07-26 15:55:37 -0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,373 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FlashCardApp
+{
+    public partial class MainWindow : Window
+    {
+        private int currentCardIndex = 0;
+        private List<FlashCard> flashCards = new List<FlashCard>();
+        private bool isShowingAnswer = false;
+        private bool isLoadingDecks = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadDeckList();
+
+            if (DatabaseExists())
+            {
+                LoadFlashCardsFromDatabase();
+                DisplayCard();
+            }
+            else
+            {
+                TextBlockQuestionAnswer.Text = "Cards: \n\n(EMPTY).";
+            }
+        }
+
+        private string GetAppFolder()
+        {
+            string appFolder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "FlashCardApp");
+
+            Directory.CreateDirectory(appFolder);
+
+            return appFolder;
+        }
+
+        private void LoadDeckList()
+        {
+            isLoadingDecks = true;
+            string appFolder = GetAppFolder();
+
+            ComboBoxDecks.Items.Clear();
+
+            string[] databases = Directory.GetFiles(appFolder, "*.db");
+
+            foreach (string database in databases)
+            {
+                ComboBoxDecks.Items.Add(Path.GetFileName(database));
+            }
+
+
+            // Select current deck
+            string currentDeckFile = Path.Combine(
+                appFolder,
+                "currentdeck.txt");
+
+
+            if (File.Exists(currentDeckFile))
+            {
+                string currentDeck = File.ReadAllText(currentDeckFile);
+
+                ComboBoxDecks.SelectedItem = currentDeck;
+            }
+            isLoadingDecks = false;
+        }
+
+        private bool DatabaseExists()
+        {
+            string appFolder = GetAppFolder();
+
+            string settingsFile = Path.Combine(appFolder, "currentdeck.txt");
+
+            if (!File.Exists(settingsFile))
+                return false;
+
+            string currentDeck = File.ReadAllText(settingsFile);
+
+            return File.Exists(Path.Combine(appFolder, currentDeck));
+        }
+
+        private void LoadFlashCardsFromDatabase()
+        {
+            try
+            {
+                string appFolder = GetAppFolder();
+
+                string currentDeck = File.ReadAllText(
+                    Path.Combine(appFolder, "currentdeck.txt"));
+
+                string dbPath = Path.Combine(appFolder, currentDeck);
+
+                string connectionString = $"Data Source={dbPath};Version=3;";
+
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM FlashCards";
+
+                    using (var command = new SQLiteCommand(query, connection))
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            flashCards.Add(new FlashCard
+                            {
+                                Question = reader["Question"]?.ToString(),
+                                Answer = reader["Answer"]?.ToString(),
+                                Hint = reader["Hint"]?.ToString()
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ButtonImportCards_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter =
+                "Supported Files (*.db;*.csv)|*.db;*.csv|" +
+                "SQLite Database (*.db)|*.db|" +
+                "CSV Files (*.csv)|*.csv|" +
+                "All Files (*.*)|*.*";
+
+            if (DatabaseExists())
+            {
+                MessageBoxResult result = MessageBox.Show(
+                    "Importing a new deck will replace the current deck. Continue?",
+                    "Replace Deck",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question);
+
+                if (result != MessageBoxResult.Yes)
+                    return;
+            }
+
+            if (dialog.ShowDialog() == true)
+            {
+                string selectedFile = dialog.FileName;
+
+                string extension = Path.GetExtension(selectedFile).ToLower();
+
+                if (extension == ".db")
+                {
+                    ImportDatabase(selectedFile);
+                }
+                else if (extension == ".csv")
+                {
+                    ImportCsv(selectedFile);
+                }
+            }
+        }
+
+        private void ImportDatabase(string selectedFile)
+        {
+            string fileName = Path.GetFileName(selectedFile);
+
+            string appFolder = GetAppFolder();
+
+            string destination = Path.Combine(appFolder, fileName);
+
+            File.Copy(selectedFile, destination, true);
+
+            File.WriteAllText(
+                Path.Combine(appFolder, "currentdeck.txt"),
+                fileName);
+
+            // Reload cards
+            flashCards.Clear();
+            currentCardIndex = 0;
+            isShowingAnswer = false;
+
+            LoadFlashCardsFromDatabase();
+            DisplayCard();
+
+            MessageBox.Show("Deck imported successfully!");
+            LoadDeckList();
+        }
+
+        private void ImportCsv(string csvFile)
+        {
+            string appFolder = GetAppFolder();
+
+            string dbName = Path.GetFileNameWithoutExtension(csvFile) + ".db";
+            string dbPath = Path.Combine(appFolder, dbName);
+
+            // Replace existing database if it already exists.
+            if (File.Exists(dbPath))
+            {
+                File.Delete(dbPath);
+            }
+
+            SQLiteConnection.CreateFile(dbPath);
+
+            string connectionString = $"Data Source={dbPath};Version=3;";
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string createTable = @"
+            CREATE TABLE FlashCards
+            (
+                Question TEXT,
+                Answer TEXT,
+                Hint TEXT
+            );";
+
+                using (var command = new SQLiteCommand(createTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                string[] lines = File.ReadAllLines(csvFile);
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] parts = lines[i].Split(',');
+
+                    if (parts.Length < 3)
+                        continue;
+
+                    string question = parts[0];
+                    string answer = parts[1];
+                    string hint = parts[2];
+
+                    string insert = @"
+                        INSERT INTO FlashCards
+                        (Question, Answer, Hint)
+                        VALUES
+                        (@Question, @Answer, @Hint);";
+
+                    using (var command = new SQLiteCommand(insert, connection))
+                    {
+                        command.Parameters.AddWithValue("@Question", question);
+                        command.Parameters.AddWithValue("@Answer", answer);
+                        command.Parameters.AddWithValue("@Hint", hint);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                File.WriteAllText(
+                        Path.Combine(appFolder, "currentdeck.txt"),
+                        dbName);
+
+                flashCards.Clear();
+                currentCardIndex = 0;
+                isShowingAnswer = false;
+
+                LoadFlashCardsFromDatabase();
+                DisplayCard();
+
+                MessageBox.Show("CSV imported successfully!");
+
+                LoadDeckList();
+            }
+        }
+
+        private void ComboBoxDecks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (isLoadingDecks)
+                return;
+
+            if (ComboBoxDecks.SelectedItem == null)
+                return;
+
+            string selectedDeck = ComboBoxDecks.SelectedItem.ToString();
+
+            string appFolder = GetAppFolder();
+
+            File.WriteAllText(
+                Path.Combine(appFolder, "currentdeck.txt"),
+                selectedDeck);
+
+
+            flashCards.Clear();
+            currentCardIndex = 0;
+            isShowingAnswer = false;
+
+            LoadFlashCardsFromDatabase();
+
+            DisplayCard();
+        }
+
+        private void DisplayCard()
+        {
+            if (flashCards.Count == 0)
+            {
+                MessageBox.Show("No flashcards found.");
+                return;
+            }
+
+            var currentCard = flashCards[currentCardIndex];
+
+            if (isShowingAnswer)
+            {
+                TextBlockQuestionAnswer.Text = currentCard.Answer;
+            }
+            else
+            {
+                TextBlockQuestionAnswer.Text = currentCard.Question;
+            }
+        }
+
+
+        private void ButtonAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            isShowingAnswer = !isShowingAnswer;
+            DisplayCard();
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            currentCardIndex = (currentCardIndex + 1) % flashCards.Count;
+            isShowingAnswer = false;
+            DisplayCard();
+        }
+
+        private void ButtonPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            currentCardIndex = (currentCardIndex - 1 + flashCards.Count) % flashCards.Count;
+            isShowingAnswer = false;
+            DisplayCard();
+        }
+
+        private void ButtonHint_Click(object sender, RoutedEventArgs e)
+        {
+            var currentCard = flashCards[currentCardIndex];
+            MessageBox.Show(currentCard.Hint, "Hint");
+        }
+    }
+
+    public class FlashCard
+    {
+        public string? Question { get; set; } = string.Empty;
+        public string? Answer { get; set; } = string.Empty;
+        public string? Hint { get; set; } = string.Empty;
+    }
+
+
+    public static class Extensions
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# FlashCard-App
+
+In this project I am creating a flash card application where you can add your own cards to study 
+
+---
+
+## Features
+
+
+---
+
+## Technologies Used
+
+
+
+---
+
+## Preview
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+
+
+   
+
